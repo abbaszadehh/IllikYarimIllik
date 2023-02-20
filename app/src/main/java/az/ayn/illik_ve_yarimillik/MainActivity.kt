f213e79d71d0f845b6947f98add6af6b3106cb0f
hasan5151 <hasanyildiz5151@gmail.com>
2023-02-20 21:33:04 +0600

    Update MainActivity.kt

--- app/src/main/java/az/ayn/illik_ve_yarimillik/MainActivity.kt
+++ app/src/main/java/az/ayn/illik_ve_yarimillik/MainActivity.kt
@@ -2,11 +2,15 @@ package az.ayn.illik_ve_yarimillik
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+
 import az.ayn.illik_ve_yarimillik.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        
         setContentView(R.layout.activity_main)
 
 
