8fbfb8f33650a72c87cf42efde8ab38c1f4e8888
abbaszadehh <aynur.abbaszadehh@gmail.com>
2023-02-20 19:37:14 +0400

    new version

--- app/src/main/java/az/ayn/illik_ve_yarimillik/MainActivity.kt
+++ app/src/main/java/az/ayn/illik_ve_yarimillik/MainActivity.kt
@@ -1,17 +1,15 @@
 package az.ayn.illik_ve_yarimillik
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-
-
-import az.ayn.illik_ve_yarimillik.fragment.MainFragment
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
         
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_main)
 
 
 
